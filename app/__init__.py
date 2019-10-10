ad263f0b1e4ef4666ca353c4825040bdaf86167c
levovit <playersoft1999@gmail.com>
2019-10-10 22:35:54 +0300

    chapter 12 date and time

--- app/__init__.py
+++ app/__init__.py
@@ -7,6 +7,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_mail import Mail
 from flask_bootstrap import Bootstrap
+from flask_moment import Moment
 from flask_login import LoginManager
 
 
@@ -16,6 +17,7 @@ db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 mail = Mail(app)
 bootstrap = Bootstrap(app)
+moment = Moment(app)
 login = LoginManager(app)
 login.login_view = 'login'
 
