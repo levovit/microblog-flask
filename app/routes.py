57cfba0ec2bf5960d3b8e3c0090f83c437a99dc9
levovit <playersoft1999@gmail.com>
2019-10-28 21:48:46 +0200

    translate now is not login required

--- app/routes.py
+++ app/routes.py
@@ -197,7 +197,6 @@ def unfollow(username):
 
 
 @app.route('/translate', methods=['POST'])
-@login_required
 def translate_text():
     return jsonify({'text': translate(request.form['text'],
                                       request.form['source_language'],
